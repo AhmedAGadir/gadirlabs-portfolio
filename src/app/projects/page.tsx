d41e2cf646d1b4199495ec8c59c4ff0c01a133e0
AhmedAGadir <ahmed.gadir@gmail.com>
2023-10-18 21:01:32 +0100

    grammar

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -50,7 +50,7 @@ const projects: {
   {
     name: 'Gap Bio',
     description:
-      'Landing page for a pharmaceutical licensing firm. Built with React, Next.js, and Tailwind',
+      'Landing page for a pharmaceutical licensing firm. Built with React, Next.js, and Tailwind.',
     link: {
       href: 'https://gapbio.netlify.app/',
       label: 'gapbio.netlify.app',
